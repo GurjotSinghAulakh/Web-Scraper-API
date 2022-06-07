5c4b30660f00315eb3699a23fb18bc59c9f5e590
mortazaBaqeri <s351899@oslomet.no>
2022-06-07 13:53:00 +0200

    Created v7
    Created V7: a scraper with multithreading and ability to scrape all undercategories of the category "Hvitevarer"

--- main.py
+++ main.py
@@ -1,17 +1,10 @@
-# from openpyxl import Workbook
-#
-# wb = Workbook()
-#
-# hei = input()
-# wb.create_sheet(hei)
-# ws = "ws_"
-# ws_brand = ws + hei
-# ws_sheet = wb[ws_brand]
-# ws_brand = ws_sheet
-#
-# print(ws_brand)
+adresse = "4319 Sandnes"
+postnr = adresse.strip().split(" ")[-2]
 
+print(postnr)
 
-pris = "10 000 kr"
-prisstrip = pris.replace(" ", "").split("kr")[0]
-print(prisstrip)
+# by = adresse.split(",")[0].strip()
+# print(by)
+#
+# postnr = by.split(" ")[-2]
+# print(postnr)
