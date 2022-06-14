9fdca2120105f9b1b2a5b653ebbdf5a507304281
mortazaBaqeri <s351899@oslomet.no>
2022-06-14 16:55:13 +0200

    Added read_excel for finding duplicate files

--- main.py
+++ main.py
@@ -1,90 +1,30 @@
-# import threading
-# from openpyxl import Workbook, load_workbook  # To create excel sheets
-#
-# wb = Workbook()
-# wb.create_sheet("Hvitevarer")
-# ws = wb["Hvitevarer"]
-# ws.append(["Varenavn", "Under kategori", "Kategori (type)", "Pris", "Merke", "Postnummer"])
-#
-# def funk(data):
-#     for i in range(6):
-#         ws.append([data])
-#         wb.save("test.xlsx")
-#
-#
-# thread1 = threading.Thread(target=funk, args=("T1",))
-# thread2 = threading.Thread(target=funk, args=("T2",))
-# thread1.start()
-# thread2.start()
-#
-# print("DONE")
+import threading
+import time
+from datetime import datetime, date   # to measure the speed of the the algorithm
+from threading import Timer
 
-# array = ["tang", "hsjdahs", "ooo"]
-# array.insert(0, "hei")
-# array.insert(0,"ahsjkdhakjsdhkjashdjkashkjhjdsa")
-# while len(array) > 4:
-#     array.pop()
-#
-# if "hei" in array:
-#     print(array)
-#     print("dublikat")
-# from datetime import date
-#
-# today = date.today()
-# name = str(today) + ".xlsx"
-# file_name = name
-#
-# print(file_name)
-# from datetime import datetime, timedelta
-#
-# n = 5
-# current_time = datetime.now()
-# future_time = current_time + timedelta(minutes=n)
-#
-# print(future_time)
-
-# filename = "Hei"
-# print("./Scrapped Data/" + filename)
+import requests  # to make request (html-request)
+from bs4 import BeautifulSoup  # to make the html code compact
+from openpyxl import Workbook  # To create excel sheets
+wb = Workbook()
+wb.create_sheet("Hvitevarer")
+ws = wb["Hvitevarer"]
+ws.append(["Varenavn", "Under kategori", "Kategori (type)", "Pris", "Merke", "Postnummer"])
 
 
-# from datetime import datetime, timedelta
-# from threading import Timer
-#
-# x = datetime.today()
-# kl00 = x.replace(day=x.day, hour=0, minute=0, second=0, microsecond=0)
-# y = kl00 + timedelta(days=1)
-# delta_t = y - kl00
-#
-# secs = delta_t.total_seconds()
-#
-# print(secs)
-#
-# def hello_world():
-#     print("hello world")
-#     #...
-#
-# t = Timer(secs, hello_world)
-# t.start()
+def save(i):
+    wb.save(f"{i}.xlsx")
 
-# import logging
-# logging.basicConfig(level=logging.INFO, filename='sample.log')
-#
-# def hypotenuse(a, b):
-#     """Compute the hypotenuse"""
-#     return (a**2 + b**2)**0.5
-#
-# kwargs = {'a':3, 'b':4, 'c':hypotenuse(3, 4)}
-#
-# logging.debug("a = {a}, b = {b}".format(**kwargs))
-# logging.info("Hypotenuse of {a}, {b} is {c}".format(**kwargs))
-# logging.warning("a={a} and b={b} are equal".format(**kwargs))
-# logging.error("a={a} and b={b} cannot be negative".format(**kwargs))
-# logging.critical("Hypotenuse of {a}, {b} is {c}".format(**kwargs))
-#
-# #> WARNING:root:a=3 and b=3 are equal
-# #> ERROR:root:a=-1 and b=4 cannot be negative
-# #> CRITICAL:root:Hypotenuse of a, b is 5.0
+threads = []
 
+for i in range(4):
+    t = threading.Thread(target=save, args=(1,))
+    t.daemon = True
+    threads.append(t)
 
-hei = "hei"
-print(hei.split(" "))
+for i in range(4):
+    threads[i].start()
+    time.sleep(2)
+
+for i in range(4):
+    threads[i].join()
--- read_excel.py
+++ read_excel.py
@@ -1,56 +1,73 @@
+# from openpyxl import Workbook, load_workbook  # To create excel sheets
+#
+# wb = load_workbook('/Users/mortaza/Downloads/2022-06-08.xlsx')
+# ws = wb["Hvitevarer"]
+#
+# andre_hvitevarer = 0
+# frysere = 0
+# innbyggingsovner = 0
+# kjøleskap = 0
+# komfyrer = 0
+# mikrobølgeovner = 0
+# oppvaskmaskiner = 0
+# platetopper = 0
+# tørketromler = 0
+# vaskemaskiner = 0
+# ventilatorer = 0
+#
+# for i in range(2, 9101):
+#     cell = f'B{i}'
+#     verdi = ws[cell].value
+#     if verdi == "andre hvitevarer":
+#         andre_hvitevarer += 1
+#     elif verdi == "frysere":
+#         frysere += 1
+#     elif verdi == "innbyggingsovner":
+#         innbyggingsovner += 1
+#     elif verdi == "kjøleskap":
+#         kjøleskap += 1
+#     elif verdi == "komfyrer":
+#         komfyrer += 1
+#     elif verdi == "mikrobølgeovner":
+#         mikrobølgeovner += 1
+#     elif verdi == "oppvaskmaskiner":
+#         oppvaskmaskiner += 1
+#     elif verdi == "platetopper":
+#         platetopper += 1
+#     elif verdi == "tørketromler":
+#         tørketromler += 1
+#     elif verdi == "vaskemaskiner":
+#         vaskemaskiner += 1
+#     elif verdi == "ventilatorer":
+#         ventilatorer += 1
+#     else:
+#         print(f"HVA FAEN {verdi}")
+#
+# print(andre_hvitevarer)
+# print(frysere)
+# print(innbyggingsovner)
+# print(kjøleskap)
+# print(komfyrer)
+# print(mikrobølgeovner)
+# print(oppvaskmaskiner)
+# print(platetopper)
+# print(tørketromler)
+# print(vaskemaskiner)
+# print(ventilatorer)
+#
 from openpyxl import Workbook, load_workbook  # To create excel sheets
+wb1 = load_workbook('/Users/mortaza/Downloads/2022-06-08.xlsx')
+ws1 = wb1["Hvitevarer"]
 
-wb = load_workbook('/Users/mortaza/Downloads/2022-06-08.xlsx')
-ws = wb["Hvitevarer"]
+wb2 = load_workbook('/Users/mortaza/Downloads/2022-06-08.xlsx')
+ws2 = wb2["Hvitevarer"]
 
-andre_hvitevarer = 0
-frysere = 0
-innbyggingsovner = 0
-kjøleskap = 0
-komfyrer = 0
-mikrobølgeovner = 0
-oppvaskmaskiner = 0
-platetopper = 0
-tørketromler = 0
-vaskemaskiner = 0
-ventilatorer = 0
+for i in range(2, 9200):
+    cell1 = f'A{i}'
+    verdi1 = ws1[cell1].value
+    for b in range(2, 9200):
+        cell2 = f'A{b}'
+        verdi2 = ws2[cell2].value
 
-for i in range(2, 9101):
-    cell = f'B{i}'
-    verdi = ws[cell].value
-    if verdi == "andre hvitevarer":
-        andre_hvitevarer += 1
-    elif verdi == "frysere":
-        frysere += 1
-    elif verdi == "innbyggingsovner":
-        innbyggingsovner += 1
-    elif verdi == "kjøleskap":
-        kjøleskap += 1
-    elif verdi == "komfyrer":
-        komfyrer += 1
-    elif verdi == "mikrobølgeovner":
-        mikrobølgeovner += 1
-    elif verdi == "oppvaskmaskiner":
-        oppvaskmaskiner += 1
-    elif verdi == "platetopper":
-        platetopper += 1
-    elif verdi == "tørketromler":
-        tørketromler += 1
-    elif verdi == "vaskemaskiner":
-        vaskemaskiner += 1
-    elif verdi == "ventilatorer":
-        ventilatorer += 1
-    else:
-        print(f"HVA FAEN {verdi}")
-
-print(andre_hvitevarer)
-print(frysere)
-print(innbyggingsovner)
-print(kjøleskap)
-print(komfyrer)
-print(mikrobølgeovner)
-print(oppvaskmaskiner)
-print(platetopper)
-print(tørketromler)
-print(vaskemaskiner)
-print(ventilatorer)
+        if verdi1 == verdi2:
+            print(f"Dette er duplikat {verdi2}")
